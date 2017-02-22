f1a522b615e76f7b9ce349924b7199e209c2c108
Mats Stijlaart <m.stijlaart@avisi.nl>
2017-02-22 09:48:30 +0100

    Measure all files

--- performance-test.sh
+++ performance-test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./node_modules/.bin/gulp elm-performance-single-file && node performance/single-file.js src/Inspection.elm
+./node_modules/.bin/gulp elm-performance-single-file && node performance/single-file.js src/Parser/Patterns.elm
--- performance/single-file.js
+++ performance/single-file.js
@@ -1,33 +1,70 @@
 const loadingPorts = require('../js/util/file-loading-ports');
 const fs = require('fs');
-const path = process.argv[2];
-const content = fs.readFileSync(path).toString();
 const Elm = require('./generated/SingleFileRead');
 const app = Elm.SingleFileRead.worker();
-const counter = 200;
+const counter = 20;
+
+var paths = require('find').fileSync(/\.elm$/, 'src');
+var content, currentFile;
 
 var results = [];
+var table = {}
 var before;
 
-function sendNext( ) {
-  before = new Date().getTime()
-  app.ports.newInput.send(content);
+function storeResult() {
+    table[currentFile] = results;
+    results = [];
+}
+
+function sendNext() {
+    before = new Date().getTime()
+    app.ports.newInput.send(content);
 };
 
+function printResults() {
+    triples = Object.keys(table).map(function(k) {
+        var list = table[k];
+        list.sort((b, a) => b - a);
+        var step = list.length / 10;
+        const part = list.slice(step, step * 9);
+        const average = part.reduce((x, y) => x + y, 0) / part.length;
+        return [k, average, list];
+    });
+
+    triples.sort((a, b) => b[1] - a[1]);
+
+    // var step =  (counter/10);
+    // const part = results.slice (step, step * 9);
+    // const average = part.reduce((x, y) => x + y, 0) / part.length;
+    // results.sort((b,a) => b - a);
+    // console.log(results);
+    // console.log(average);
+    triples.forEach(x => {
+        console.log(x[1], x[0]);
+    })
+}
+
+function triggerNextFile() {
+    const next = paths.shift();
+    if (!next) {
+        printResults();
+        return;
+    }
+    console.log('Next file:', next);
+    currentFile = next;
+    content = fs.readFileSync(next).toString();
+    sendNext();
+}
 app.ports.parsed.subscribe(function(_) {
-  var after = new Date().getTime();
-  console.log("Tick");
-  results.push(after-before);
-  if (results.length >= counter) {
-    var step =  (counter/10);
-    const part = results.slice (step, step * 9);
-    const average = part.reduce((x, y) => x + y, 0) / part.length;
-    results.sort((b,a) => b - a);
-    console.log(results);
-    console.log(average);
-    return;
-  }
-  sendNext();
+    var after = new Date().getTime();
+    console.log("Tick");
+    results.push(after - before);
+    if (results.length >= counter) {
+        storeResult();
+        triggerNextFile();
+        return;
+    }
+    sendNext();
 });
 
-sendNext();
+triggerNextFile();
