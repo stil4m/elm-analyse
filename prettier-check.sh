784bc169205f5fcd1d201e44b5e415dccd36b44e
Mats Stijlaart <m.stijlaart@avisi.nl>
2017-11-08 13:44:43 +0100

    Issue #121: JS - Use single quotes

--- prettier-check.sh
+++ prettier-check.sh
@@ -2,7 +2,7 @@
 jsfiles=$(find js -name "*.js" ! -path "js/backend-elm.js" ! -path "js/public/client-elm.js" ! -path "js/public/sigma/*"  | grep '\.jsx\?$' | tr '\n' ' ')
 [ -z "$jsfiles" ] && exit 0
 
-diffs=$(node_modules/.bin/prettier --tab-width 4 -l $jsfiles)
+diffs=$(node_modules/.bin/prettier --single-quote --tab-width 4 -l $jsfiles)
 [ -z "$diffs" ] && exit 0
 
 echo $diffs
