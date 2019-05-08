0678a427b91ecf31c9fafd48215e9d897361c2b1
antew <antewcode@gmail.com>
2019-05-07 23:17:07 -0400

    Make sure the cache directory is local to the project. This is needed to support workspace styles features in editors like VSCode, for instance you may have a project with an elixir frontend in /myproject/backend, and Elm code in /myproject/frontend/, in the editor you might open /myproject/ to jump between projects easier. This adds a new LocalCache class that takes the project path as an argument and stores the elm-analyse cache relative to that.

--- ts/fileReader.ts
+++ ts/fileReader.ts
@@ -1,8 +1,71 @@
 // Reference the module
 import * as fs from 'fs';
-import * as cache from './util/cache';
+import { LocalCache } from './util/cache';
 const sums = require('sums');
 import { FileContent } from './domain';
+
+export class FileReader {
+    private cache: LocalCache;
+
+    constructor(cache: LocalCache) {
+        this.cache = cache;
+        cache.setupShaFolder();
+    }
+
+    private readFileNotCached(
+        realPath: string,
+        path: string,
+        checksum: string
+    ) {
+        return new Promise((accept: (data: FileContent) => void) => {
+            fs.readFile(realPath, function(e, content) {
+                if (e) {
+                    accept(errorResponse(path));
+                    return;
+                }
+                const originalContent = content.toString();
+
+                accept({
+                    success: true,
+                    path: path,
+                    sha1: checksum,
+                    content: originalContent,
+                    ast: null
+                });
+            });
+        });
+    }
+
+    public readFile(
+        directory: string,
+        path: string,
+        cb: (data: FileContent) => void
+    ) {
+        var real = directory + '/' + path;
+
+        sums.checksum(fs.createReadStream(real))
+            .then(
+                (checkSumResult: { sum: string }) => {
+                    const checksum = checkSumResult.sum;
+
+                    if (this.cache.hasAstForSha(checksum)) {
+                        return {
+                            success: true,
+                            path: path,
+                            sha1: checksum,
+                            content: fs.readFileSync(real).toString(),
+                            ast: this.cache.readAstForSha(checksum)
+                        };
+                    }
+                    return this.readFileNotCached(real, path, checksum);
+                },
+                function() {
+                    return errorResponse(path);
+                }
+            )
+            .then(cb);
+    }
+}
 
 function errorResponse(path: string): FileContent {
     return {
@@ -13,54 +76,3 @@ function errorResponse(path: string): FileContent {
         ast: null
     };
 }
-
-function readFileNotCached(realPath: string, path: string, checksum: string) {
-    return new Promise((accept: (data: FileContent) => void) => {
-        fs.readFile(realPath, function(e, content) {
-            if (e) {
-                accept(errorResponse(path));
-                return;
-            }
-            const originalContent = content.toString();
-
-            accept({
-                success: true,
-                path: path,
-                sha1: checksum,
-                content: originalContent,
-                ast: null
-            });
-        });
-    });
-}
-
-function readFile(directory: string, path: string, cb: (data: FileContent) => void) {
-    var real = directory + '/' + path;
-
-    sums
-        .checksum(fs.createReadStream(real))
-        .then(
-            (checkSumResult: { sum: string }) => {
-                const checksum = checkSumResult.sum;
-
-                if (cache.hasAstForSha(checksum)) {
-                    return {
-                        success: true,
-                        path: path,
-                        sha1: checksum,
-                        content: fs.readFileSync(real).toString(),
-                        ast: cache.readAstForSha(checksum)
-                    };
-                }
-                return readFileNotCached(real, path, checksum);
-            },
-            function() {
-                return errorResponse(path);
-            }
-        )
-        .then(cb);
-}
-
-cache.setupShaFolder();
-
-export { readFile };
--- ts/util/cache.ts
+++ ts/util/cache.ts
@@ -4,10 +4,39 @@ import * as fsExtra from 'fs-extra';
 // const fsExtra = require('fs-extra');
 const osHomedir = require('os-homedir');
 import * as path from 'path';
-const cachePath = path.resolve('elm-stuff', '.elm-analyse');
 const packageJsonPath = path.resolve(__dirname, '..', '..', '..', 'package.json');
 const elmAnalyseVersion = require(packageJsonPath).version;
 
+class LocalCache {
+    private cachePath: string;
+    constructor(projectPath: string) {
+        this.cachePath = path.join(projectPath, 'elm-stuff', '.elm-analyse');
+    }
+    public storeShaJson(sha1: string, content: JSON) {
+        fs.writeFile(
+            path.resolve(this.cachePath, '_shas', sha1 + '.json'),
+            JSON.stringify(content),
+            function() {}
+        );
+    }
+    public elmCachePathForSha(sha: string) {
+        return path.resolve(this.cachePath, '_shas', sha + '.elma');
+    }
+
+    public astCachePathForSha(sha: string): string {
+        return path.resolve(this.cachePath, '_shas', sha + '.json');
+    }
+    public setupShaFolder(): void {
+        fsExtra.ensureDirSync(path.resolve(this.cachePath, '_shas'));
+    }
+    public hasAstForSha(sha: string): boolean {
+        return fs.existsSync(this.astCachePathForSha(sha));
+    }
+
+    public readAstForSha(sha: string): string {
+        return fs.readFileSync(this.astCachePathForSha(sha)).toString();
+    }
+}
 var major: string;
 if (elmAnalyseVersion.split('.')[0] === '0') {
     major = '0.' + elmAnalyseVersion.split('.')[1];
@@ -56,34 +85,10 @@ function storeDependencyJson(dependency: string, version: string, content: strin
     fs.writeFile(targetPath, content, function() {});
 }
 
-function elmCachePathForSha(sha: string) {
-    return path.resolve(cachePath, '_shas', sha + '.elma');
-}
-
-function astCachePathForSha(sha: string): string {
-    return path.resolve(cachePath, '_shas', sha + '.json');
-}
-
-function hasAstForSha(sha: string): boolean {
-    return fs.existsSync(astCachePathForSha(sha));
-}
-
-function readAstForSha(sha: string): string {
-    return fs.readFileSync(astCachePathForSha(sha)).toString();
-}
-
-function setupShaFolder(): void {
-    fsExtra.ensureDirSync(path.resolve(cachePath, '_shas'));
-}
-
 export {
-    storeShaJson,
     readDependencyJson,
     readPackageDependencyInfo,
     storePackageDependencyInfo,
     storeDependencyJson,
-    hasAstForSha,
-    elmCachePathForSha,
-    readAstForSha,
-    setupShaFolder
+    LocalCache
 };
