1e136657ea137e0f63b5bf70ae38ec1e6da994be
antew <antewcode@gmail.com>
2019-05-30 22:02:15 -0400

    Read elm-analyse configuration from the directory where the elm.json is rather than the current working directory of the process.  This helps with running in a multi-project structure where you may open "/project/" in an editor and have an elm project under "/project/web/".

--- ts/ports/context.ts
+++ ts/ports/context.ts
@@ -1,13 +1,14 @@
 import * as fs from 'fs';
 import * as fileGatherer from '../util/file-gatherer';
 import { ElmApp, Context } from '../domain';
+import * as path from 'path';
 
 function setup(app: ElmApp, directory: string) {
     app.ports.loadContext.subscribe(() => {
         const input = fileGatherer.gather(directory);
         var configuration;
         try {
-            configuration = fs.readFileSync('./elm-analyse.json').toString();
+            configuration = fs.readFileSync(path.join(directory, 'elm-analyse.json')).toString();
         } catch (e) {
             configuration = '';
         }
