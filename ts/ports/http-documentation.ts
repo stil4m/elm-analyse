753146addc8b1fa4e22646b9b76e4d7a6a942b9f
Gábor Kerekes <krks.gbr@gmail.com>
2020-03-19 17:22:12 +0100

    Attempt to load docs.json from $ELM_HOME before package site

--- ts/ports/http-documentation.ts
+++ ts/ports/http-documentation.ts
@@ -1,35 +1,48 @@
 import * as request from 'request';
+import * as path from 'path';
+import * as os from 'os';
+
 import { ElmApp, DependencyPointer } from '../domain';
 
-function setup(app: ElmApp) {
-    app.ports.loadHttpDocumentation.subscribe((pointer: DependencyPointer) => {
-        const { name, version } = pointer;
+function loadFromPackageSite({ name, version }: DependencyPointer, cb: (result: any) => void) {
+    request.get(`http://package.elm-lang.org/packages/${name}/${version}/docs.json`, function(
+        err: any,
+        _response: request.Response,
+        body: string
+    ) {
+        if (err) {
+            cb(null);
+        }
+        try {
+            const parsed = JSON.parse(body);
+            cb(parsed);
+        } catch (e) {
+            cb(null);
+        }
+    });
+}
 
-        request.get(`http://package.elm-lang.org/packages/${name}/${version}/docs.json`, function(
-            err: any,
-            _response: request.Response,
-            body: string
-        ) {
-            if (err) {
-                app.ports.onHttpDocumentation.send({
-                    dependency: pointer,
-                    json: null
-                });
-                return;
-            }
-            try {
-                const parsed = JSON.parse(body);
-                app.ports.onHttpDocumentation.send({
-                    dependency: pointer,
-                    json: parsed
-                });
-            } catch (e) {
-                app.ports.onHttpDocumentation.send({
-                    dependency: pointer,
-                    json: null
-                });
-            }
-        });
+function loadFromElmHome({ name, version }: DependencyPointer, directory: string) {
+    const projectElmJson = path.resolve(directory, 'elm.json');
+    const elmVersion = require(projectElmJson)['elm-version'];
+    const elmHome = process.env.ELM_HOME || path.resolve(os.homedir(), '.elm');
+    const docsJsonPath = path.resolve(elmHome, elmVersion, 'package', name, version, 'docs.json');
+    return require(docsJsonPath);
+}
+
+function setup(app: ElmApp, directory: string) {
+    app.ports.loadDocsJson.subscribe((pointer: DependencyPointer) => {
+        const onLoaded = (json: any) =>
+            app.ports.onDocsJsonLoaded.send({
+                dependency: pointer,
+                json
+            });
+        try {
+            const docs = loadFromElmHome(pointer, directory);
+            onLoaded(docs);
+        } catch (e) {
+            loadFromPackageSite(pointer, onLoaded);
+        }
     });
 }
 
